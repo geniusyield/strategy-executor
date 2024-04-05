5c98d7bf7db722da6d0327338302316646e1d964
4TT1L4 <2914096+4TT1L4@users.noreply.github.com>
2024-04-05 22:56:36 +0200

    Polish SDK #4

--- app.py
+++ app.py
@@ -19,6 +19,28 @@ import logging
 app = Flask(__name__)
 logger = logging.getLogger('gunicorn.error')
 
+class Api:
+    def __init__(self, client):
+        self.client = client
+
+    def place_order(self, offered_amount, offered_token, price_token, price_amount):
+        body: post_order_parameters = post_order_parameters.PostOrderParameters()
+        body.offer_amount=offered_amount
+        body.offer_token=offered_token
+        body.price_token=price_token
+        body.price_amount=price_amount
+        response: Response[settings] = post_orders.sync_detailed(client=self.client, body=body)
+        return response
+
+    def cancel_order(self, order_reference):
+          body: delete_order_parameters = delete_order_parameters.DeleteOrderParameters()
+          body.address="addr_test1qz9zh342r6ynfhk974tmjxxxznrmmh0tre09tdh6gc3r6r2rq3uxt0yu4c0mg2ck6h8f0h3ykh7n4w68f7dr3mfch58q6rhtxg"
+          body.order_references=[order_reference]
+          response: Response[settings] = delete_orders.sync_detailed(client=self.client, body=body)
+          return response
+
+# Other methods can be added as needed
+
 
 def check_env_variable(var_name):
     if var_name not in os.environ:
@@ -53,124 +75,53 @@ def worker():
     # TEST ############################################
     client = AuthenticatedClient(base_url="http://server:8082/v0/", token=SERVER_API_KEY, auth_header_name="api-key", prefix="")
     with client as client:
-        
         attempt_successful = False
+        logger.info(f" Connecting to backend at: {BACKEND_URL}...")
         while not attempt_successful:
             try:
                 response: Response[settings] = get_settings.sync_detailed(client=client)
-                logger.info(f"GET SETTINGS RESPONSE STATUS CODE: {response.status_code}")
-                logger.info(response)
+                logger.info(f" [OK] Backend is available at: {BACKEND_URL} ✅ ")
+                logger.info(f" > Version: {response.parsed.version}")
+                logger.info(f" > Backend: {response.parsed.backend}")
+                logger.info(f" > Revision: {response.parsed.revision}")
+                logger.info(f" > Address: {response.parsed.address}")
                 attempt_successful = True
             except Exception as e:
                 # If an exception occurs, print the message and wait for 5 seconds
-                logger.info(f"Backend not available. Wait {RETRY_DELAY} seconds")
+                logger.info(f"Backend not available. Wait {RETRY_DELAY} seconds...")
                 logger.debug(e)
                 time.sleep(RETRY_DELAY)
                 # The loop will then automatically retry
-        mode="place"
-        # Place order:
-        if mode == "place":
-          body: post_order_parameters = post_order_parameters.PostOrderParameters()
-          body.offer_amount="1"
-          body.offer_token="lovelace"
-          body.price_token="66a524d7f34d954a3ad30b4e2d08023c950dfcd53bbe3c2314995da6.744d454c44"
-          body.price_amount="1"
-          body.address="addr_test1qz9zh342r6ynfhk974tmjxxxznrmmh0tre09tdh6gc3r6r2rq3uxt0yu4c0mg2ck6h8f0h3ykh7n4w68f7dr3mfch58q6rhtxg"
-          response: Response[settings] = post_orders.sync_detailed(client=client, body=body)
-          logger.info(f"POST RESPONSE STATUS CODE: {response.status_code}")
-          logger.info(f"POST PARSED RESPONSE: {response.parsed}")
-          logger.info(response)
-        else:
-          # Cancel order:
-          body: delete_order_parameters = delete_order_parameters.DeleteOrderParameters()
-          body.address="addr_test1qz9zh342r6ynfhk974tmjxxxznrmmh0tre09tdh6gc3r6r2rq3uxt0yu4c0mg2ck6h8f0h3ykh7n4w68f7dr3mfch58q6rhtxg"
-          body.order_references=["bab1d0d115bce1afadf789728f5039df0e5edceb05fa78f62a16926c92336cde#0"]
-          response: Response[settings] = delete_orders.sync_detailed(client=client, body=body)
-          logger.info(f"DELETE RESPONSE STATUS CODE: {response.status_code}")
-          logger.info(f"DELETE PARSED RESPONSE: {response.parsed}")
-          logger.info(response)
+
+        api_client = Api(client);
+        logger.info("==============================================")
+        logger.info("[OK] Initialization is done ✅ ")
     
-    logger.info(f">> FINISHED <<")
-    time.sleep(1000)
-    logger.info(f">>STOPPED <<")
-    # TEST############################################
-
-    logger.info("Worker thread is starting...")
-    logger.info(f"Wait {STARTUP_DELAY}s until backend is ready...")
-    time.sleep(STARTUP_DELAY)
-    logger.info(" [OK] Waiting is over.")
-    logger.info("Starting strategy....")
-
-    # Create API client:
-    configuration = swagger_client.Configuration()
-    configuration.api_key['api-key'] = SERVER_API_KEY
-    configuration.host = BACKEND_URL
-
-    api_client= swagger_client.ApiClient(configuration)
-
-    assets_api = swagger_client.AssetsApi(api_client)
-    balances_api = swagger_client.BalancesApi(api_client)
-    historical_prices_api = swagger_client.HistoricalPricesApi(api_client)
-    markets_api = swagger_client.MarketsApi(api_client)
-    order_book_api = swagger_client.OrderBookApi(api_client)
-    orders_api = swagger_client.OrdersApi(api_client)
-    settings_api = swagger_client.SettingsApi(api_client)
-    trading_fees_api = swagger_client.TradingFeesApi(api_client)
-    transaction_api = swagger_client.TransactionApi(api_client)
-
-    client = {}
-    client["assets"] = assets_api
-    client["balances"] = balances_api
-    client["historical_prices"] = historical_prices_api
-    client["markets"] = markets_api
-    client["order_book"] = order_book_api
-    client["orders"] = orders_api
-    client["settings"] = settings_api
-    client["trading_fees"] = trading_fees_api
-    client["transaction"] = transaction_api
-
-    attempt_successful = False
-
-    while not attempt_successful:
-        try:
-            resp = client["settings"].v0_settings_get()
-            attempt_successful = True  # If success, update the flag to exit the loop
-        except Exception as e:
-            # If an exception occurs, print the message and wait for 5 seconds
-            logger.info(f"Backend not available. Wait {RETRY_DELAY} seconds")
-            logger.debug(e)
-            time.sleep(RETRY_DELAY)
-            # The loop will then automatically retry
-
-    logger.info(f" > Using version {resp.version} of {resp.backend}.")
-    logger.info("==============================================")
-    logger.info("[OK] Initialization is done ✅ ")
-
-    logger.info(f"Loading strategy {STRATEGY}")
-    strategy_class_ref = load_strategy(STRATEGY)
-    strategy = strategy_class_ref(client, CONFIG, logger)
-    logger.info(f" [OK] Strategy is loaded.")
-
-    while True:
-      logger.info("==============================================")
-      logger.info(f" > Invoking strategy ({STRATEGY})... ⚙️⏳ ")
-      start_time = time.time()
-      logger.info(f"Start time: {datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')}")
-      try:
-          strategy.execute(client, CONFIG, logger)
-      except Exception as e:
-          logger.error(f" ❌ Strategy exeuction has failed with an exception. ❌ ")
-          logger.error(" ❌ Exception : %s\n" % e)
-      else:
-          logger.info(f"[OK] Strategy exeuction has been finished ✅ ")
-      finally:
-          end_time = time.time()
-          execution_time = end_time - start_time
-          logger.info(f"End time: {datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')}")
-          logger.info(f"Execution time: {execution_time:.4f} seconds")
-
-      logger.info(f"Wait {EXECUTION_DELAY}s until next execution...")
-      time.sleep(EXECUTION_DELAY)
+        logger.info(f"Loading strategy {STRATEGY}")
+        strategy_class_ref = load_strategy(STRATEGY)
+        strategy = strategy_class_ref(client, CONFIG, logger)
+        logger.info(f" [OK] Strategy is loaded.")
+    
+        while True:
+          logger.info("==============================================")
+          logger.info(f" > Invoking strategy ({STRATEGY})... ⚙️⏳ ")
+          start_time = time.time()
+          logger.info(f"Start time: {datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')}")
+          try:
+              strategy.execute(api_client, CONFIG, logger)
+          except Exception:
+              logger.error(f" ❌ Strategy exeuction has failed with an exception. ❌ ")
+              logger.exception(" ❌ Exception occurred:")
+          else:
+              logger.info(f"[OK] Strategy exeuction has been finished ✅ ")
+          finally:
+              end_time = time.time()
+              execution_time = end_time - start_time
+              logger.info(f"End time: {datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')}")
+              logger.info(f"Execution time: {execution_time:.4f} seconds")
+    
+          logger.info(f"Wait {EXECUTION_DELAY}s until next execution...")
+          time.sleep(EXECUTION_DELAY)
 
 if __name__ == 'app':
     logger.info("==============================================")
--- strategies/strategy_a.py
+++ strategies/strategy_a.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 
 class strategy_a:
-    def __init__(self, client, CONFIG, logger):
+    def __init__(self, api_client, CONFIG, logger):
         self.start_time = datetime.now()
         self.counter = 0
         self.last_execution_time = None
-        print("Strategy A instance created")
+        self.last_order_ref=None
+        logger.info(" > init: strategy_a instance created.")
 
-    def execute(self, client, CONFIG, logger):
+    def execute(self, api_client, CONFIG, logger):
         current_time = datetime.now()
 
         if self.last_execution_time is None:
@@ -23,11 +24,33 @@ class strategy_a:
         self.counter += 1
         logger.info(f" > Counter: {self.counter}")
 
-        api_response = client["settings"].v0_settings_get()
-
-        logger.info(f"Version: {api_response.version}")
-        logger.info(f"setting_1: {CONFIG['setting_1']}")
-        logger.info(f"setting_2: {CONFIG['setting_2']}")
-
-        logger.info(f"Create order")
-        client["orders"].v0_settings_get()
+        if self.last_order_ref == None:
+            # No order was placed -> place order.
+            logger.info(f" > PLACING ORDER....")
+            response = api_client.place_order(
+                         offered_amount="1",
+                         offered_token="lovelace",
+                         price_token="66a524d7f34d954a3ad30b4e2d08023c950dfcd53bbe3c2314995da6.744d454c44",
+                         price_amount="1"
+            )
+            logger.info(f" > RESPONSE STATUS CODE: {response.status_code}")
+            if response.status_code == 200:
+                logger.info(f"order_ref: {response.parsed.order_ref}")
+                self.last_order_ref=response.parsed.order_ref
+            
+        else:
+            # An order has already been placed -> cancel it.
+            ref = self.last_order_ref
+            shortened_ref = f"{ref[:8]}...{ref[-8:]}" if len(ref) > 16 else ref
+            logger.info(f" > CANCELING {shortened_ref}")
+            response = api_client.cancel_order(order_reference=self.last_order_ref)
+            logger.info(f" > RESPONSE STATUS CODE: {response.status_code}")
+            if response.status_code == 200:
+                logger.info(f" > [OK] cancelled: {shortened_ref}")
+            else:
+                logger.info(f" > [FAILED] could not cancel: {shortened_ref} ❌")
+            
+            # Reset the reference, so we place a new order.
+            self.last_order_ref=None
+        
+        logger.info(f" > DONE.")
