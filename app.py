3a54d070ed3e63494ce02c2c9de2acd22a95c280
4TT1L4 <2914096+4TT1L4@users.noreply.github.com>
2024-04-15 15:56:20 +0200

    fix: hardcoded-backend-url

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def load_strategy(strategy_class):
     return strategy_class_ref
 
 def worker():
-    client = AuthenticatedClient(base_url="http://server:8082/v0/", token=SERVER_API_KEY, auth_header_name="api-key", prefix="")
+    client = AuthenticatedClient(base_url=f"{BACKEND_URL}/v0/", token=SERVER_API_KEY, auth_header_name="api-key", prefix="")
     with client as client:
         attempt_successful = False
         own_address : str = ""
